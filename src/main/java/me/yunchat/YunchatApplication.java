97330edb4fb2eaf4d71f7a5c35e28acca2786f97
barami62 <wodbs7758@naver.com>
2023-07-01 14:00:32 +0900

    project setting

---
+++ src/main/java/me/yunchat/YunchatApplication.java
@@ -0,0 +1,13 @@
+package me.yunchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yunchat/YunchatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yunchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
