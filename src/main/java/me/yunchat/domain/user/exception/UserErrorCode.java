a9ce9ea31dbd308d313fc6bb4c503a421a135d4b
barami62 <wodbs7758@naver.com>
2023-07-02 16:16:54 +0900

    feat : web 도메인 추가 & 로그인 관련 에러코드, exception 을 이동

--- src/main/java/me/yunchat/domain/user/exception/UserErrorCode.java
+++ src/main/java/me/yunchat/domain/user/exception/UserErrorCode.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode {
-    NOT_SAME_PASSWORD("확인 비밀번호가 다릅니다."),
     NO_USER_NICKNAME("유저 닉네임이 존재하지 않습니다.");
 
     private final String statusMessage;
