3992412f02ee2b33542b5d7eb26abd57cbd7b3f4
barami62 <wodbs7758@naver.com>
2023-07-02 16:21:50 +0900

    feat : user 도메인은 독립적이고, web 도메인이 user 도메인에 종속적이도록 수정

--- src/main/java/me/yunchat/domain/user/dto/UserSaveDto.java
+++ src/main/java/me/yunchat/domain/user/dto/UserSaveDto.java
@@ -2,12 +2,15 @@ package me.yunchat.domain.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
-public class SignUpRequestDto {
+public class UserSaveDto {
 
     private String nickname;
     private String password;
-    private String confirmPassword;
 }
