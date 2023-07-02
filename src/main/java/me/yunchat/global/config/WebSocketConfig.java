8dea8c72971fe0c9e4944080ab20fa2586b3f46a
barami62 <wodbs7758@naver.com>
2023-07-02 21:16:31 +0900

    refactor : stomp config 를 global 디렉토리로 이동

--- src/main/java/me/yunchat/global/config/WebSocketConfig.java
+++ src/main/java/me/yunchat/global/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package me.yunchat.domain.stomp.config;
+package me.yunchat.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
